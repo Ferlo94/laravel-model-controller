b49e4595d7d28e968ba3e7449a1c0e26f484a824
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-07-11 17:03:47 +0200

    red scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Movies</title>
 </head>
 <body>
@@ -13,7 +14,7 @@
             <li> 
                 <h3>Titolo:</h3>{{$movie->title}}
                 <h5>Titolo originale:</h5>{{$movie['original_title']}}
-                <h5>Paese:</h5>{{$movie['nationality']}}
+                <h5>Nazionalita':</h5>{{$movie['nationality']}}
                 <h5>Data di uscita:</h5>{{$movie['date']}}
                 <h5>Voto:</h5>{{$movie['vote']}}
             </li>
